09e95519fb10fc7a290abbc13b1317b93e938f56
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2026-01-12 10:50:41 +0900

    [Init] UE 프로젝트 추가

---
+++ Source/UnrealNetwork/UnrealNetwork.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UnrealNetwork : ModuleRules
+{
+	public UnrealNetwork(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"UnrealNetwork",
+			"UnrealNetwork/Variant_Platforming",
+			"UnrealNetwork/Variant_Platforming/Animation",
+			"UnrealNetwork/Variant_Combat",
+			"UnrealNetwork/Variant_Combat/AI",
+			"UnrealNetwork/Variant_Combat/Animation",
+			"UnrealNetwork/Variant_Combat/Gameplay",
+			"UnrealNetwork/Variant_Combat/Interfaces",
+			"UnrealNetwork/Variant_Combat/UI",
+			"UnrealNetwork/Variant_SideScrolling",
+			"UnrealNetwork/Variant_SideScrolling/AI",
+			"UnrealNetwork/Variant_SideScrolling/Gameplay",
+			"UnrealNetwork/Variant_SideScrolling/Interfaces",
+			"UnrealNetwork/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/UnrealNetwork/UnrealNetwork.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealNetwork.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealNetwork, "UnrealNetwork" );
+
+DEFINE_LOG_CATEGORY(LogUnrealNetwork)
